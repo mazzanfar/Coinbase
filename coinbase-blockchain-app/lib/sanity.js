531a65a4e5b7b10dded399e8fa3af648530a4dad
Maximilian Ghazanfar <maximilian.ghazanfar@kcl.ac.uk>
2022-01-30 19:30:07 -0500

    Setup project

---
+++ coinbase-blockchain-app/lib/sanity.js
@@ -0,0 +1,13 @@
+import sanityClient from '@sanity/client'
+
+export const client = sanityClient ({
+
+    projectId: 'knsuitxf',
+    dataset: 'production',
+    apiVersion: '2021-03-25',
+    token: 
+        'skEQGV38MRX1YXBzShZhZVqLIlM05Kaboli59LCutevEpflhUEdIwrplLCNCW6YSQY2DYVA8W0ndglQWZ9p7UUUFgMVGRRtGqWbl5c1wVN6cxK8OI1X3uzmxnGV883tWhhYJLoWhI5Xvwezbd0e0J7H7eO4SLBQT85E8gO7dVX34FMAPxgWs',
+
+    useCdn: false,
+
+})
